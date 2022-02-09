2831dc34e6026eedec46e52aa88fd95976da227f
miridk <Rinkovski@live.dk>
2022-02-09 07:07:47 +0100

    Corrected dotnet6 webapi with tags and deleted unused files here and there

---
+++ Web_Api_CRUD-Template-dotnet6/Web_Api_CRUD-Template-dotnet6 - With tags/src/Content/Web_Api_CRUD-Template-dotnet6/Migrations/20220205223415_CreateInitial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web_Api_CRUD_Template_dotnet6.Migrations
+{
+    public partial class CreateInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Templates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TAG_{'propertiesEf'}
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Templates", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Templates");
+        }
+    }
+}
---
+++ Web_Api_CRUD-Template-dotnet6/Web_Api_CRUD-Template-dotnet6 - With tags/src/Content/Web_Api_CRUD-Template-dotnet6/Template.cs
@@ -0,0 +1,8 @@
+﻿namespace Web_Api_CRUD_Template_dotnet6
+{
+    public class Template
+    {
+        public int Id { get; set; }
+        TAG_{'propertiesModel'}
+    }
+}
